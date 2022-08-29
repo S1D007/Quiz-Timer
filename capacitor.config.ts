ecf5909dd848e2427365ac6436eced16aece7936
Siddhant <s1d.official011001011@gmail.com>
2022-08-29 14:10:29 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'quiz-timer',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
